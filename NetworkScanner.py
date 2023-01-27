7df455b9e021171675c175ec938a605b1fbccf24
Akash Falaskar <123051087+Mrblackhat80@users.noreply.github.com>
2023-01-27 11:51:48 +0530

    Update NetworkScanner.py

--- NetworkScanner.py
+++ NetworkScanner.py
@@ -1,11 +1,11 @@
-import scapy.all as scapy
-import re
-import time
-import pyfiglet
+import scapy.all as scapy # importing scapy module
+import re # importing regular expression module
+import time # importing time module
+import pyfiglet # importing pyfiglet module
 text=pyfiglet.figlet_format("Mr.Blackhat")
 print(text)
 
-ipregex=re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$")
+ipregex=re.compile("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]*$") # regular expression to match IP address range
 
 print("__"*30)
 print("[*]Welcome Buddy....")
@@ -19,14 +19,14 @@ Enter IP Address Range: 192.168.0.0/24
 print("__"*30)
 
 try:
-    IP=input("\n[*]Enter IP Address Range: ") 
+    IP=input("\n[*]Enter IP Address Range: ") # user input for IP address range
 except KeyboardInterrupt:
     time.sleep(0.75)
     print("\n\n[*]Quitting...............")
     exit()
 
 while True:
-    if ipregex.search(IP):
+    if ipregex.search(IP): # check if input matches the regular expression for IP address range
         print(f"{IP} is a Valid IP address range")
         break
-Result=scapy.arping(IP)
+Result=scapy.arping(IP) # use scapy's arping function to ping the IP address range
